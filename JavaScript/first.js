fc735368e2e034289258b85630d59acaa382feeb
umairhassan1567 <uh72375@gmail.com>
2025-07-29 08:00:15 +0500

    First Commit

---
+++ JavaScript/first.js
@@ -0,0 +1,26 @@
+console.log('Firsr');
+console.log('ali');
+age=24
+console.log(age)
+age='ali'
+console.log(age)
+var age='ali'
+console.log(age)
+var age='allli'
+console.log(age)
+let agee=12; // now the variable cannot be declared again but can be updated
+agee='Umair'
+console.log(agee)
+typeof age;
+typeof agee;
+let isTrue=false;
+typeof isTrue;
+let x;
+typeof x;
+let y=null;
+typeof y;
+let a=BigInt("123");
+typeof a;
+let b=Symbol("672");
+typeof b;
+
